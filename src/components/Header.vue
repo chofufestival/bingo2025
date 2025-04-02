436200cbf3c50e6b1448c45adffc404ad6182d8e
Laddge <dev.laddge@gmail.com>
2025-04-03 08:09:18 +0900

    feat: 画面のレイアウトを変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/vue'
 
 <template>
   <div class="flex p-6 justify-center relative">
-    <h1 class="font-bold text-2xl">○調Bingo</h1>
+    <h1 class="font-bold text-3xl">○調Bingo</h1>
     <div class="absolute top-0 right-0 flex items-center h-full p-6">
       <button class="btn btn-ghost px-2">
         <Icon icon="mdi:history" class="size-8" />
